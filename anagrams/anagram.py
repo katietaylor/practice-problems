2341ee1f6b5083381d45a415f0ed83da9326efe5
Katie <knt.taylor@gmail.com>
2017-05-31 23:31:07 +0000

    solved anagram challenge

---
+++ anagrams/anagram.py
@@ -0,0 +1,48 @@
+"""Given a list of words, return the word with the most anagrams.
+
+For a list of ['act', 'cat', 'bill']:
+- 'act' and 'cat' are anagrams, so they both have 2 matching words.
+- 'bill' has no anagrams, os it has one matching word (itself).
+
+Given that 'act' is the first instance of the most-anagrammed word,
+we return that.
+
+    >>> find_most_anagrams_from_wordlist(['act', 'cat', 'bill'])
+    'act'
+
+Let's use a file of words where each line is a word:
+
+    >>> all_words = [w.strip() for w in open('words.txt')]
+    >>> find_most_anagrams_from_wordlist(all_words)
+    'angor'
+
+"""
+
+
+def find_most_anagrams_from_wordlist(wordlist):
+    """Given list of words, return the word with the most anagrams."""
+
+    anagrams = {}
+
+    for word in wordlist:
+        sorted_word = ''.join(sorted(list(word)))
+
+        anagrams[sorted_word] = anagrams.get(sorted_word, []) + [word]
+
+    most = None
+
+    for k, v in anagrams.items():
+
+        if not most:
+            most = (k, v)
+
+        elif len(v) > len(most[1]):
+                most = (k, v)
+
+    return most[1][0]
+
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. YAY!\n"
